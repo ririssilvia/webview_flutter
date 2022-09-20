2c6a7413ca35145b52ef7e3d94e85ff00dfc4c77
ririssilvia <64761595+ririssilvia@users.noreply.github.com>
2022-09-20 13:06:19 +0700

    Mengelola Cookie

--- lib/src/menu.dart
+++ lib/src/menu.dart
@@ -5,20 +5,85 @@ import 'package:webview_flutter/webview_flutter.dart';
 enum _MenuOptions {
   navigationDelegate,
   userAgent,
-  javascriptChannel, // Add this line
+  javascriptChannel,
+  // Add from here ...
+  listCookies,
+  clearCookies,
+  addCookie,
+  setCookie,
+  removeCookie,
+  // ... to here.
 }
 
 class Menu extends StatefulWidget {
-  const Menu({required this.controller, super.key});
+  // Convert to StatefulWidget
+  const Menu({required this.controller, Key? key}) : super(key: key);
 
   final Completer<WebViewController> controller;
 
   @override
-  State<Menu> createState() => _MenuState();
+  State<Menu> createState() => _MenuState(); // Add this line
 }
 
 class _MenuState extends State<Menu> {
-  @override
+  // New State class
+  final CookieManager cookieManager = CookieManager(); // Add this line
+
+  Future<void> _onListCookies(WebViewController controller) async {
+    final String cookies =
+        await controller.runJavascriptReturningResult('document.cookie');
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(cookies.isNotEmpty ? cookies : 'There are no cookies.'),
+      ),
+    );
+  }
+
+  Future<void> _onClearCookies() async {
+    final hadCookies = await cookieManager.clearCookies();
+    String message = 'There were cookies. Now, they are gone!';
+    if (!hadCookies) {
+      message = 'There were no cookies to clear.';
+    }
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+      ),
+    );
+  }
+
+  Future<void> _onAddCookie(WebViewController controller) async {
+    await controller.runJavascript('''var date = new Date();
+  date.setTime(date.getTime()+(30*24*60*60*1000));
+  document.cookie = "FirstName=John; expires=" + date.toGMTString();''');
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('Custom cookie added.'),
+      ),
+    );
+  }
+
+  Future<void> _onSetCookie(WebViewController controller) async {
+    await cookieManager.setCookie(
+      const WebViewCookie(name: 'foo', value: 'bar', domain: 'flutter.dev'),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('Custom cookie is set.'),
+      ),
+    );
+  }
+
+  Future<void> _onRemoveCookie(WebViewController controller) async {
+    await controller.runJavascript(
+        'document.cookie="FirstName=John; expires=Thu, 01 Jan 1970 00:00:00 UTC" ');
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('Custom cookie removed.'),
+      ),
+    );
+  }
+ @override
   Widget build(BuildContext context) {
     return FutureBuilder<WebViewController>(
       future: widget.controller.future,
@@ -27,30 +92,44 @@ class _MenuState extends State<Menu> {
           onSelected: (value) async {
             switch (value) {
               case _MenuOptions.navigationDelegate:
-                await controller.data!.loadUrl('https://youtube.com');
+                controller.data!.loadUrl('https://youtube.com');
                 break;
               case _MenuOptions.userAgent:
                 final userAgent = await controller.data!
                     .runJavascriptReturningResult('navigator.userAgent');
-                if (!mounted) return;
                 ScaffoldMessenger.of(context).showSnackBar(SnackBar(
                   content: Text(userAgent),
                 ));
                 break;
-              // Add from here ...
               case _MenuOptions.javascriptChannel:
                 await controller.data!.runJavascript('''
-                  var req = new XMLHttpRequest();
-                  req.open('GET', "https://api.ipify.org/?format=json");
-                  req.onload = function() {
-                    if (req.status == 200) {
-                      let response = JSON.parse(req.responseText);
-                      SnackBar.postMessage("IP Address: " + response.ip);
-                    } else {
-                      SnackBar.postMessage("Error: " + req.status);
-                    }
+                var req = new XMLHttpRequest();
+                req.open('GET', "https://api.ipify.org/?format=json");
+                req.onload = function() {
+                  if (req.status == 200) {
+                    let response = JSON.parse(req.responseText);
+                    SnackBar.postMessage("IP Address: " + response.ip);
+                  } else {
+                    SnackBar.postMessage("Error: " + req.status);
                   }
-                  req.send();''');
+                }
+                req.send();''');
+                break;
+              // Add from here ...
+              case _MenuOptions.clearCookies:
+                _onClearCookies();
+                break;
+              case _MenuOptions.listCookies:
+                _onListCookies(controller.data!);
+                break;
+              case _MenuOptions.addCookie:
+                _onAddCookie(controller.data!);
+                break;
+              case _MenuOptions.setCookie:
+                _onSetCookie(controller.data!);
+                break;
+              case _MenuOptions.removeCookie:
+                _onRemoveCookie(controller.data!);
                 break;
               // ... to here.
             }
@@ -64,10 +143,30 @@ class _MenuState extends State<Menu> {
               value: _MenuOptions.userAgent,
               child: Text('Show user-agent'),
             ),
-            // Add from here ...
             const PopupMenuItem<_MenuOptions>(
               value: _MenuOptions.javascriptChannel,
               child: Text('Lookup IP Address'),
+            ),
+            // Add from here ...
+            const PopupMenuItem<_MenuOptions>(
+              value: _MenuOptions.clearCookies,
+              child: Text('Clear cookies'),
+            ),
+            const PopupMenuItem<_MenuOptions>(
+              value: _MenuOptions.listCookies,
+              child: Text('List cookies'),
+            ),
+            const PopupMenuItem<_MenuOptions>(
+              value: _MenuOptions.addCookie,
+              child: Text('Add cookie'),
+            ),
+            const PopupMenuItem<_MenuOptions>(
+              value: _MenuOptions.setCookie,
+              child: Text('Set cookie'),
+            ),
+            const PopupMenuItem<_MenuOptions>(
+              value: _MenuOptions.removeCookie,
+              child: Text('Remove cookie'),
             ),
             // ... to here.
           ],
